c464b06382da21fc15c6c5c9e3995ff1ebebf4d6
XTreme.ws <XTreme.ws>
2016-05-08 22:48:34 +0300

    my divider

---
+++ ConsoleApplication6/ConsoleApplication6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+
+
+    class Program
+    {
+        static void Divider(int x)
+        {
+            for (int i = 0; i < x; i++)
+            {
+                if ((x % (i + 1)) == 0)
+                {
+                    Console.WriteLine(i+1);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+           //  В качестве аргумента ввести нужное число и в консоли выведутся все целочисленные делители
+                Divider(2);
+               Console.ReadKey();
+
+        }
+    }
+}
